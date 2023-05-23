2589784ea2f1325c427f7c0a8271fc04ab5ab23a
Edouard Duplessis <eduplessis@fixrs.ca>
2023-05-23 08:56:58 -0400

    first

---
+++ src/collections/Users.ts
@@ -0,0 +1,26 @@
+import { CollectionConfig } from 'payload/types';
+
+const Users: CollectionConfig = {
+  slug: 'users',
+  auth: true,
+  admin: {
+    useAsTitle: 'email',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    // Email added by default
+    {
+      name: 'name',
+      type: 'text',
+    },
+    {
+      name: 'user_image',
+      type: 'upload',
+      relationTo: 'media'
+    }
+  ],
+};
+
+export default Users;
